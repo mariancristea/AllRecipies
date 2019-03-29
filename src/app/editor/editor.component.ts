b478c65c19824617f6c765ce10850c4b577b2c82
mcristea <Marian.Cristea@guillemot.ro>
2019-03-29 15:50:53 +0200

    Various

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -25,13 +25,15 @@ export class EditorComponent implements OnInit {
         private fb: FormBuilder
     ) {
         this.recipeForm = this.fb.group({
-            'title': [''],
+            'title': ['', Validators.required],
             'image': [''],
             'description': [''],
             'prep-time': [''],
             'cook-time': [''],
             'servings': [''],
             'ingredients': [''],
+            'category': ['', Validators.required],
+            'cuisine': ['', Validators.required]
         });
 
       }  
@@ -42,24 +44,29 @@ export class EditorComponent implements OnInit {
     }
 
     submitForm() {
-        this.isSubmitting = true;
-        
-        for (const [key, value] of Object.entries(this.recipeForm.value)) {
-            if(key != 'ingredients') this.recipe[key] = value;
-          }
-        var x = this.recipeForm.value.ingredients;
-        var y = x.split('\n');
-        console.log(Object.keys(y).length);
-        this.recipe.tagList = [];
-        for(var i = 0; i < Object.keys(y).length; i++) {
-            this.recipe.tagList.push(y[i]);
+        if(this.recipeForm.valid)
+        {
+            this.isSubmitting = true;
+            console.log(this.recipeForm.valid);
+            
+            for (const [key, value] of Object.entries(this.recipeForm.value)) {
+                if(key != 'ingredients') this.recipe[key] = value;
+              }
+            var x = this.recipeForm.value.ingredients;
+            var y = x.split('\n');
+            console.log(Object.keys(y).length);
+            this.recipe.tagList = [];
+/*             for (var i = 0; i < Object.keys(y).length; i++) {
+                this.recipe.tagList.push(y[i]);
+            } */
+            this.recipe.tagList.push(this.recipeForm.value.category);
+            this.recipe.tagList.push(this.recipeForm.value.cuisine);
+           
+            this.recipeService
+                .save(this.recipe)
+                .subscribe(recipe => this.router.navigateByUrl('/recipe/' + recipe.slug));
         }
-        this.recipe.tagList.push(this.categoryFormControl.value);
-        this.recipe.tagList.push(this.cuisineFormControl.value);
        
-        this.recipeService
-            .save(this.recipe)
-            .subscribe(recipe => this.router.navigateByUrl('/recipe/' + recipe.slug));
     }
    
     updateRecipe(values: Object) {
